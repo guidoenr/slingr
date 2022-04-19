b2b22cf6d92320a47a157c7105a85da1e03525c1
guido <guido@debian.guido>
2022-04-19 11:55:08 -0300

    guido@debian quick-commit

--- go/main.go
+++ go/main.go
@@ -9,12 +9,12 @@ Executable: generates a file that we can run (like this one)
 Reusable: code used as 'helpers' good place to put reusable logic, if we
 are coding some kind of libraries or maybe a code to share with our friends
 we gonna use the 'reusable' packages, not using the 'package main' at the top
-something like 'package guido-tools' or whatelse 
+something like 'package guido-tools' or whatelse
 
 */
 package main //so if the package is exectuable, there will be a 'package main' at the top of the file
 
-import "fmt"
+import "fmt" // a library to import debuggin and printing funcs
 
 func main() {
 	fmt.Println("Hi there!")
