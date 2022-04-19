126308a84c9fa4b09cbf3024e44f8e511a1f7b58
guido <guido@debian.guido>
2022-04-19 12:57:06 -0300

    guido@debian quick-commit

---
+++ go/cards/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// deckSize := 20
+// variables can be initialized outside of a function, but cannot be assigned a variable
+// the correct way will be deckSize
+var deckSize int
+
+func main() {
+	//  var card string = "Ace of Spades"  this is STATIC, this will never contain a number
+	/*
+		this line is the same that the one above, i'ts also static, but with the difference
+		that GO automaticlly infers the variable type (string)
+		and this line, also works for initializing and assigning a new value to a variable
+	*/
+	card := "Ace of spades" // MOST USED
+	// the ':=' its only used when we are declaring and defining a new variable in one line
+	// if we use only the '=' we are making an assigment to an existing variable
+	//card = "Five of Diamonds"
+	new_card := newCard()
+
+	fmt.Println(new_card)
+	fmt.Println(card)
+}
+
+// always put the data type in return-type func
+func newCard() string {
+	return "Five of diamonds"
+}
+
+
